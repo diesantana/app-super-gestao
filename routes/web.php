1d1dcd9742cce0a48a29757039a6814058768f66
Diego Santana <3c.santana@gmail.com>
2026-03-10 17:08:46 -0300

    feat: Cria as rotas login, clients, suppliers e products

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'main']);
-
 Route::get('/contact', [ContactController::class, 'contact']);
-
 Route::get('/about', [AboutController::class, 'about']);
+Route::get('/login', function() {return 'Tela de login';});
+Route::get('/clients', function() {return 'Tela de clientes';});
+Route::get('/suppliers', function() {return 'Tela de fornecedores';});
+Route::get('/products', function() {return 'Tela de produtos';});
