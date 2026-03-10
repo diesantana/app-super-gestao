0f5ed9fc4619a8512eef7a0210c8ff5996d1a705
Diego Santana <3c.santana@gmail.com>
2026-03-10 09:19:02 -0300

    feat: Adiciona os controllers as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'main']);
+
+Route::get('/contact', [ContactController::class, 'contact']);
+
+Route::get('/about', [AboutController::class, 'about']);
