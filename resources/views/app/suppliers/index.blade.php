036703f2bb43ee73739c7ca22d578261bd11bcb0
Diego Santana <3c.santana@gmail.com>
2026-03-11 13:11:38 -0300

    feat: Cria a view index  para os Fornecedores

---
+++ resources/views/app/suppliers/index.blade.php
@@ -0,0 +1,9 @@
+<h3>Fornecedores</h3>
+{{ 'Texto será exibido'}}
+{{-- Texto será comendado --}}
+<p>Tela de fornecedores</p>
+
+@php
+    // Código PHP
+    echo '<p> Código PHP aqui </p>';
+@endphp
