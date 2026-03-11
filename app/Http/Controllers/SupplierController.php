13d0ca41213e99339f371b547ee85dd351168998
Diego Santana <3c.santana@gmail.com>
2026-03-11 13:10:51 -0300

    feat: Cria o controlador para os Fornecedores

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SupplierController extends Controller
+{
+    function index(){
+        return view('app.suppliers.index');
+    }
+}
