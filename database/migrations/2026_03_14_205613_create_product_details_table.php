64d36872a95b746132fcbdbd36525a8ed8e62e85
Diego Santana <3c.santana@gmail.com>
2026-03-14 18:01:15 -0300

    feat: Definindo o relacionamento 1-1 nas migrations

---
+++ database/migrations/2026_03_14_205613_create_product_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->id();
+            $table->float('comprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+            // foreign key
+            $table->foreignId('product_id')->unique()->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_details');
+    }
+};
