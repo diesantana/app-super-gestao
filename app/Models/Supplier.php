ef7e5e12ca2b9fb8a0230efaf589e514802bc302
Diego Santana <3c.santana@gmail.com>
2026-03-16 20:03:13 -0300

    feat: Trabalhando com seeders e factories

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Supplier extends Model
 {
-    //
+    protected $fillable = ['nome', 'uf', 'email'];
+    use HasFactory;
 }
